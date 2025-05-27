29def40b5a24062b7da7cc3e7b8780b6049d05f2
bhakyagajendran@gmail.com <bhakyagajendran@gmail.com>
2025-05-27 11:00:00 +0530

    Bhakya added Model and DTO

--- Stock_Portfolio_Management_Application/src/main/java/com/stockapp/dto/AuthResponse.java
+++ Stock_Portfolio_Management_Application/src/main/java/com/stockapp/dto/AuthResponse.java
@@ -1,5 +1,16 @@
 package com.stockapp.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
+	private String username;
+	private String message;
+	private String role;
 
 }
