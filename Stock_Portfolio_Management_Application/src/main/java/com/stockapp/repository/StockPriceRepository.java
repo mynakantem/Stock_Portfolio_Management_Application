ee04802cadecfd2249a84c36e8a6e541f710a519
sreayas <sreayas@gmail.com>
2025-05-29 16:24:50 +0530

    sreayas added price-fetcher service test and updated repository

--- Stock_Portfolio_Management_Application/src/main/java/com/stockapp/repository/StockPriceRepository.java
+++ Stock_Portfolio_Management_Application/src/main/java/com/stockapp/repository/StockPriceRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface StockPriceRepository extends JpaRepository<StockPrice, String>{
+	Optional<StockPrice> findBySymbol(String stockSymbol);
 }
